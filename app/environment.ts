9fb5a2e3b842fa0d1a928896567a7c1580ab870f
Thales César <thalesgiriboni@gmail.com>
2019-10-23 20:06:14 -0300

    Fix environment setup for production

--- app/environment.ts
+++ app/environment.ts
@@ -2,15 +2,13 @@ import { environment as devEnvironment } from './environment.dev';
 import { environment as prodEnvironment } from './environment.prod';
 
 export const environment = (() => {
-    if (
-        typeof process !== 'undefined' && process &&
-        Object.prototype.hasOwnProperty.call(process, 'env') && process.env &&
-        Object.prototype.hasOwnProperty.call(process.env, 'environment') && process.env.environment
-    ) {
+    try {
         switch (process.env.environment) {
             case 'prod':
                 return prodEnvironment;
         }
+    } catch {
+        return devEnvironment;
     }
     return devEnvironment;
 })();
